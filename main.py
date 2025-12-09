7e9498725eb6fe86fade7095a9cea91efb95c5a1
BobbySaikong <warisan6660@gmail.com>
2025-12-10 01:55:19 +0800

    added sendPhoto/Video/Image, added telegram bot, recieved apikey,chatID

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
-def sendRequest(path):
-    print(path)
+import requests
+
+CHAT_ID = ""
+TOKEN = ""
 
 
-sendRequest("afsdfsdfdsfdsfdsfdsfvfdvdfmmbdomvfkom")
+def send_message(text):
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    data = {"chat_id": CHAT_ID, "text": text}
+    r = requests.post(url, data=data)
+    print(r.json())  # debug response
+
+
+def send_photo(path, caption=""):
+    url = f"https://api.telegram.org/bot{TOKEN}/sendPhoto"
+    with open(path, "rb") as img:
+        files = {"photo": img}
+        data = {"chat_id": CHAT_ID, "caption": caption}
+        requests.post(url, files=files, data=data)
+
+
+def send_video(path, caption=""):
+    url = f"https://api.telegram.org/bot{TOKEN}/sendVideo"
+    with open(path, "rb") as vid:
+        files = {"video": vid}
+        data = {"chat_id": CHAT_ID, "caption": caption}
+        requests.post(url, files=files, data=data)
+
+
+send_video("C:/Users/HP/Downloads/bobby2.mp4", "jahak ni")
